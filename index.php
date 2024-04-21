d6887783a242af6ca717efa096f5784f2921db7a
Colton Matthews <Matthews.Colton@student.greenriver.edu>
2024-04-20 20:47:18 -0400

    added personalinfo

--- index.php
+++ index.php
@@ -11,11 +11,41 @@ $f3 = Base::instance();
 
 //deine rout
 $f3->route('GET /', function (){
-    //echo '<h1> Hello from dinner </h1>';
+    //echo '<h1> Application </h1>';
     //render view page
     $view = new Template();
-    echo $view->render('views/home-page.html');
+   echo $view->render('views/home.html');
 });
 
-$f3->run();
+$f3->route('GET|POST /personal-info', function ($f3){
+   // echo '<h1>personal info</h1>';
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
 
+
+
+
+}
+    $view = new Template();
+    echo $view->render('views/personal-info.html');
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// keep at bottom
+$f3->run();
